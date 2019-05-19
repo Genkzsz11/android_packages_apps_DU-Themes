4e685be63f6727d5eef57b3896f2cd02905cedb2
Kshitij Gupta <kshitijgm@gmail.com>
2019-05-20 02:32:20 +0530

    DUThemes: Introduce RGB accenter [2/2] *Adapt for DU Themes
    Change-Id: Ifc30e05ec689c22c891ddcd2522ad99a9a0782ca
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- Android.mk
+++ Android.mk
@@ -33,4 +33,6 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 include vendor/du/support/common.mk
 include frameworks/base/packages/SettingsLib/common.mk
 
+include vendor/du/support/common.mk
+
 include $(BUILD_PACKAGE)
